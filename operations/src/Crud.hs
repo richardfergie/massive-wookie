7e6fcedcd2508bface78508f021bb40e33cbc895
Richard Fergie <richard.fergie@gmail.com>
2016-10-25 12:31:39 +0100

    Interpreter that logs to stdout

--- operations/src/Crud.hs
+++ operations/src/Crud.hs
@@ -77,6 +77,28 @@ stateCrud p = case Op.view p of
       True -> insertWorld worldGroups x >>= stateCrud . ps
       False -> throwError $ ForeignKeyMissing "At least one group member doesn't exist"
 
+stateCrudPrint :: (Monad m, MonadError CRUDError m, MonadIO m) => CRUD a -> StateT World m a
+stateCrudPrint p = case Op.view p of
+  Return x -> return x
+  (GetProject x :>>= ps) -> (liftIO $ putStrLn "Get Project") >> getWorld worldProjects x >>= stateCrudPrint . ps
+  (GetGroup x :>>= ps) -> (liftIO $ putStrLn "Get Group") >> getWorld worldGroups x >>= stateCrudPrint . ps
+  (GetGroupMember x :>>= ps) -> (liftIO $ putStrLn "Get Groupmember") >> getWorld worldGroupMembers x >>= stateCrudPrint . ps
+  (CreateProject x :>>= ps) -> do
+    liftIO $ putStrLn "Create Project"
+    --also need facilitator check here
+    mgroup <- stateCrudPrint $ getGroup $ group x
+    case mgroup of
+      Just _ -> insertWorld worldProjects x >>= stateCrudPrint . ps
+      Nothing -> throwError $ ForeignKeyMissing $ show (group x)++" does not exist"
+  (CreateGroupMember x :>>= ps) -> (liftIO $ putStrLn "Create GroupMember") >> insertWorld worldGroupMembers x >>= stateCrudPrint . ps
+  (CreateGroup x :>>= ps) -> do
+    liftIO $ putStrLn "Create Group"
+    --also need org check here
+    mgmids <- stateCrudPrint $ traverse getGroupMember $ members x
+    case all isJust mgmids of
+      True -> insertWorld worldGroups x >>= stateCrudPrint . ps
+      False -> throwError $ ForeignKeyMissing "At least one group member doesn't exist"
+
 
 example :: CRUD ProjectId
 example = do
@@ -86,4 +108,3 @@ example = do
   Entity g _ <- createGroup $ Group 1 $ map entityKey [g1,g2,g3]
   Entity pid _ <- createProject $ Project 1 g Nothing Created
   return pid
-
